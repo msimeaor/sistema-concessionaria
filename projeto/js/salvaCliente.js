49261377042a414bba14afd43b7a1f4506270ee8
Matheus <maatsimeao@gmail.com>
2023-05-05 16:53:07 -0300

    classe para salvar cliente no banco de dados criada

---
+++ projeto/js/salvaCliente.js
@@ -0,0 +1,60 @@
+export default class SalvaCliente {
+  constructor(entradasFormulario, urlAPI, botaoAction) {
+    this.urlAPI = urlAPI
+    this.entradasFormulario = document.querySelectorAll(entradasFormulario)
+    this.botaoAction = document.querySelector(botaoAction)
+    this.dadosCliente = {}
+
+    this.init()
+  }
+
+  adcEventoBotaoAction() {
+    this.botaoAction.addEventListener('click', this.handleBotaoAction)
+  }
+
+  handleBotaoAction(event) {
+    event.preventDefault()
+    this.preencherObjetoDadosCliente()
+    this.fazerFetchAPI()
+  }
+
+  preencherObjetoDadosCliente() {
+    this.entradasFormulario.forEach(entrada => {
+      const nome = entrada.name
+      const valor = entrada.value
+      this.dadosCliente[`${nome}`] = valor
+    })
+  }
+
+  fazerFetchAPI() {
+    fetch(this.urlAPI, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.dadosCliente)
+    })
+
+    .then(response => response.json())
+    .then(cliente => {
+      console.log(cliente);
+    })
+    
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
+  fazerBindDosEventos() {
+    this.handleBotaoAction = this.handleBotaoAction.bind(this)
+  }
+
+  init() {
+    if (this.entradasFormulario.length && this.urlAPI && this.botaoAction) {
+      this.fazerBindDosEventos()
+      this.adcEventoBotaoAction()
+    } else {
+      console.log('Erro ao carregar o arquivo salvaCliente.js');
+    }
+  }
+}
