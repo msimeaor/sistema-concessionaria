1e6a7ed74ed49a0fe3823860d6ee2cfa38f07431
Matheus <maatsimeao@gmail.com>
2023-05-11 11:30:05 -0300

    classe para buscar cliente adicionada

--- projeto/js/script.js
+++ projeto/js/script.js
@@ -1,5 +1,6 @@
 import SalvaCliente from "./salvaCliente.js";
 import ValidaFormulario from "./regexValidaFormulario.js";
+import BuscaCliente from "./buscaCliente.js";
 
 // INSTANCIA DAS CLASSES
 const validaFormulario = new ValidaFormulario('[data-form="cadastro-cliente"]')
@@ -7,6 +8,13 @@ const validaFormulario = new ValidaFormulario('[data-form="cadastro-cliente"]')
 fetch('/projeto/json/clienteAPI.json')
   .then(response => response.json())
   .then(data => {
-    const {save} = data
-    const salvaCliente = new SalvaCliente('[data-form="cadastro-cliente"] .data-container', save, '.btn-salvar-dados-pessoais')
+    const { save } = data
+    const salvaCliente = new SalvaCliente('[data-form="cadastro-cliente"] .data-container', '[data-form="cadastro-cliente"] .id-cliente', '.btn-salvar-dados-pessoais', save)
+  })
+
+fetch('/projeto/json/clienteAPI.json')
+  .then(response => response.json())
+  .then(data => {
+    const { getByCpf } = data
+    const buscaCliente = new BuscaCliente('[data-form="busca-cliente"] #cpf-busca', '.lista-dados-cliente', '[data-form="busca-cliente"] .btn-buscar-cliente', getByCpf)
   })
