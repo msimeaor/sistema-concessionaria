328b3a413e217fdd2d3f12a67ad2a94731ae7958
Matheus <maatsimeao@gmail.com>
2023-05-15 13:46:18 -0300

    alguns ajustes feitos

--- projeto/js/script.js
+++ projeto/js/script.js
@@ -3,26 +3,17 @@ import ValidaFormulario from "./regexValidaFormulario.js";
 import BuscaCliente from "./buscaCliente.js";
 import AtualizaCliente from "./atualizaCliente.js";
 
+function fazerFetchURLs(endp, Classe) {
+  fetch('/projeto/json/clienteAPI.json')
+    .then(response => response.json())
+    .then(data => {
+      const endpoint = data[endp]
+      const classe = new Classe(endpoint)
+    })
+}
+
 // INSTANCIA DAS CLASSES
-const validaFormulario = new ValidaFormulario('[data-form="cadastro-cliente"]')
-
-fetch('/projeto/json/clienteAPI.json')
-  .then(response => response.json())
-  .then(data => {
-    const { save } = data
-    const salvaCliente = new SalvaCliente('[data-form="cadastro-cliente"] .data-container', '[data-form="cadastro-cliente"] .id-cliente', '.btn-salvar-dados-pessoais', save)
-  })
-
-fetch('/projeto/json/clienteAPI.json')
-  .then(response => response.json())
-  .then(data => {
-    const { getByCpf } = data
-    const buscaCliente = new BuscaCliente('[data-form="busca-cliente"] #cpf-busca', '.lista-dados-cliente', '[data-form="busca-cliente"] .btn-buscar-cliente', getByCpf)
-  })
-
-fetch('/projeto/json/clienteAPI.json')
-  .then(response => response.json())
-  .then(data => {
-    const { update } = data
-    const atualizaCliente = new AtualizaCliente(update)
-  })
+const validaFormulario = new ValidaFormulario()
+fazerFetchURLs('save', SalvaCliente)
+fazerFetchURLs('getByCpf', BuscaCliente)
+fazerFetchURLs('update', AtualizaCliente)
